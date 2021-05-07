094218267dbc01a4e5afaa9ae6be91ec27ec47fa
Matthew <matthew@tests-MacBook-Air.local>
2021-05-07 08:51:47 -0400

    testing out variables with console.log

---
+++ script.js
@@ -0,0 +1,26 @@
+// Assignment code here
+//Set Glbal variables
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numVal = ['1','2','3','4','5','6','7','8','9','0'];
+var speChar = ['!','#','$','%','^','&','*','(',')','@','?','+','=','<','>','/'];
+
+//checking to see if the array is setup correctly
+console.log(speChar [3]);
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
